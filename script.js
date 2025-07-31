9464d03f9e85dbf6b32f3eb0fec5b643ad1e58d4
Bhuvan <bhuvanmbhuvanm15@gmail.com>
2025-07-31 21:55:13 +0530

    update3

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-// Dark mode toggle
-const toggleBtn = document.getElementById('theme-toggle');
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-  toggleBtn.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
-});
-
-// Typing animation
-const typed = new Typed('#typed', {
-  strings: ['Web Developer',  'Tech Enthusiast'],
-  typeSpeed: 50,
-  backSpeed: 30,
+// Typed.js
+const typed = new Typed("#typed", {
+  strings: ["Web Developer", "Problem Solver", "Tech Enthusiast"],
+  typeSpeed: 70,
+  backSpeed: 40,
   loop: true
 });
 
-// Scroll animations
+// AOS
 AOS.init();
+
+// Theme toggle
+const toggleBtn = document.getElementById("theme-toggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggleBtn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
+
+
+emailjs.init("MJEoLTV0GtTkbfuLf"); // from EmailJS dashboard
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('service_cbzfppd', 'template_wdps48s', this)
+    .then(function() {
+      alert("Message sent successfully!");
+    }, function(error) {
+      alert("Failed to send message: " + error.text);
+    });
+});
